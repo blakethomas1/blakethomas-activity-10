2d2aa7bcdafe142db3b713240005a18bc6118885
unknown <blakethomas@mail.weber.edu>
2021-04-04 16:13:58 -0600

    final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -8,3 +8,56 @@ import 'bootstrap'
 // JavaScript
 
 //TODO
+
+import { movies } from './movies (1).js'
+
+let featured_movie = document.querySelector('.featured')
+for(let m of movies){
+    let movie_thumbnail = document.getElementById('m' + m.id)
+    movie_thumbnail.innerHTML = `
+    <img src="${m.poster}">
+    `
+
+    movie_thumbnail.onclick = function(){
+        selectMovie(m)
+    }
+}
+
+function selectMovie(m){
+    featured_movie.innerHTML = `
+    <img src="${m.poster}" style="float: left;">
+    <h1>${m.title}</h1>
+    <p>${m.plot}</p>
+    <p style="font-weight: bold;">Year: ${m.year}</p>
+    <p style="font-weight: bold;">Rated: ${m.rated}</p>
+    <p style="font-weight: bold;">Genre: ${m.Genre}</p>
+    <p style="font-weight: bold;">Rating: ${m.rating}</p>
+    <p style="font-weight: bold;">Votes: ${m.votes}</p>
+    <p style="font-weight: bold;">IMDBID: ${m.imdbID}</p>
+    `
+}
+
+function searchMovies(event){
+    if(event){
+        event.preventDefault()
+    }
+
+    let input = document.querySelector('[type="search"]').value || ""
+    for(let m of movies){
+        let movie_thumbnail = document.getElementById('m' + m.id)
+        if(m.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
+           // movie_thumb.classList.add('hidden')
+           movie_thumbnail.style.display = 'none'
+        }
+        else{
+            //movie_thumb.classList.remove('hidden')
+            movie_thumbnail.style.display = 'block'
+        }
+    }
+
+}
+
+document.querySelector('button').onclick = searchMovies
+document.querySelector('[type="search"]').onsearch = searchMovies
+document.forms[0].addEventListener('submit', searchMovies, false)
+
